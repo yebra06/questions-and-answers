fc33a56aecb358298ad6c11c2dda4ca01f446d9d
yebra06 <a.yebra022@gmail.com>
2019-07-02 00:15:45 -0500

    Add get by id question endpoint.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { Question } = require('./models');
 const router = express.Router();
 
-mongoose.connect('mongodb://localhost:27017/questions', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/questions', { useNewUrlParser: true, useFindAndModify: false })
     .then(() => console.log('connection to db successful'))
     .catch((err) => console.log(err));
 
@@ -19,9 +19,23 @@ const findAllQuestions = async (req, res) => {
   }
 };
 
+const getQuestionById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let question = await Question.findById(id);
+
+    res.status(200).json({ ok: true, question });
+  } catch (err) {
+    res.status(400).json(createErr(err));
+  }
+}
+
 const createQuestion = async (req, res) => {
   try {
-    res.status(200).json({ ok: true, question: await Question.create(req.body) });
+    const question = await Question.create(req.body);
+
+    res.status(200).json({ ok: true, question });
   } catch (err) {
     res.status(400).json(createErr(err));
   }
@@ -30,6 +44,7 @@ const createQuestion = async (req, res) => {
 const updateQuestion = async (req, res) => {
   try {
     await Question.findOneAndUpdate({ _id: req.params.id }, req.body.question);
+
     res.status(200).json({ ok: true });
   } catch (err) {
     res.status(400).json(createErr(err));
@@ -37,11 +52,11 @@ const updateQuestion = async (req, res) => {
 }
 
 router.route('/questions').get(findAllQuestions).post(createQuestion);
-router.route('/questions/:id').put(updateQuestion);
+router.route('/questions/:id').get(getQuestionById).put(updateQuestion);
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
-
 app.listen(3019, () => console.log(`Listening on port:${3019}`));
